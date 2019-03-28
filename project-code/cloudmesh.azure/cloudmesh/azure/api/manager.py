bdae9dce7cd871adce6c1097793d02979cad619e
juaco77 <juaco77@me.com>
2019-03-28 18:24:35 -0400

    Update manager.py

--- project-code/cloudmesh.azure/cloudmesh/azure/api/manager.py
+++ project-code/cloudmesh.azure/cloudmesh/azure/api/manager.py
@@ -1,7 +1,16 @@
+import cloudmesh.vm.provider.azure.Provider
+
 class Manager(object):
 
     def __init__(self):
         print("init {name}".format(name=self.__class__.__name__))
 
-    def list(self, parameter):
-        print("list", parameter)
+    def list(self, service):
+		provider = None
+		self.service = self.service.lower()
+		
+		if self.service == "azure":
+			provider = cloudmesh.vm.provider.azure.Provider.Provider()
+		
+		return provider
+		
